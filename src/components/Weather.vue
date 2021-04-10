5b2767af8e587053f0e381bb2ac9eea1f61dd055
David Joao Da Silva Vicente <david.vicente@siemens.com>
2021-04-10 15:57:27 +0100

    Initial commit

---
+++ src/components/Weather.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ collectionName }}: {{ readersNumber }} {{ book.title }}</div>
+  <input type="text" v-model="book.title"/>
+
+</template>
+
+<script>
+  import { ref, reactive } from 'vue'
+
+  export default {
+    props: {
+      collectionName: String
+    },
+    setup(props) {
+      const readersNumber = ref(props.collectionName.length)
+      const book = reactive({ title: 'Vue 3 Guide' })
+
+      // expose to template
+      return {
+        readersNumber,
+        book
+      }
+    }
+  }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    // ...
+  }
+  // ...
+}
