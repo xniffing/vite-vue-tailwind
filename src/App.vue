e406f73c5813fe8e418906abb449aa5658822296
xniffing <djvicente@gmail.com>
2021-04-10 19:51:10 +0000

    color changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-<div class="bg-indigo-700 py-5 mx-auto">
-    <p class="text-center text-xl font-bold text-gray-100">{{title}}</p>
+<div class="bg-gray-100 py-5 mx-auto">
+    <img class="mx-auto" src="src/assets/logo.png" alt="">
+    <p class="text-center text-xl font-bold text-gray-700">{{title}}</p>
 </div>
 <div class="w-1/2 mx-auto text-center mt-[100px]">
-<button @click="buttonClick++" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+<button @click="buttonClick++" class="bg-green-700 hover:bg-green-600 text-gray-100 font-bold py-2 px-4 rounded">
   Clicked {{buttonClick}}
 </button>
 </div>
