90170a4affdbcdabf6c6baba2d0b6421dda39e2d
xniffing <djvicente@gmail.com>
2021-04-10 19:30:02 +0000

    app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-
-
-
+<div class="bg-indigo-700 py-5 mx-auto">
+    <p class="text-center text-xl font-bold text-gray-100">{{title}}</p>
+</div>
+<div class="">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+</div>
 </template>
 
 <script>
-
-
+export default {
+  setup() {
+    const title = 'Vue 3 + Tailwind + Vite'
+    return {
+      title
+    }
+  }
+}
 </script>
-
 <style>
-
+html {
+    @apply bg-gray-700
+}
 </style>
